2853ad301d3ce76c3e226c0f67ef0e0630461b3b
GertenP <145342311+GertenP@users.noreply.github.com>
2023-10-09 13:25:58 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,8 +1,8 @@
 function checkTemp(temp){
   if (temp <= 20 && temp >= 0){
-    console.log(-1)}
+    return -1}
   else if (temp > 21 && temp <= 40){
-    console.log(0)}
+    return 0}
   else if (temp > 40 && temp <= 60){
-    console.log(1)}
+    return 1}
 }
